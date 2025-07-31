a37e5e02cfae3109c04ab94ec59522937d50ef78
dominic-black <dominicblack.dev@gmail.com>
2025-07-31 21:01:27 +1000

    fix screen scrollviews

--- components/shared/Screen/Screen.tsx
+++ components/shared/Screen/Screen.tsx
@@ -9,7 +9,7 @@ export const Screen = ({
 }) => {
   return (
     <main className="flex-1 gap-16 bg-background pt-16 lg:pt-0 lg:pl-[100px] min-h-screen font-[family-name:var(--font-inter)]">
-      <div className="flex flex-col gap-10 p-10 max-h-screen overflow-scroll">
+      <div className="custom-scrollbar flex flex-col gap-10 p-10 max-h-screen overflow-y-auto">
         <ScreenHeader heading={heading} />
         {children}
       </div>
