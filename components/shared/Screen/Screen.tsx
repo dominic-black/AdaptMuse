f7f8e23bf5c3975371bab6134f22105d77786fd9
Dominic Black <77794795+Dec0yyy@users.noreply.github.com>
2025-08-01 20:35:05 +1000

    fix screen component

--- components/shared/Screen/Screen.tsx
+++ components/shared/Screen/Screen.tsx
@@ -8,7 +8,7 @@ export const Screen = ({
   heading: string;
 }) => {
   return (
-    <main className="pb-[max(1rem,env(safe-area-inset-bottom))] pt-[max(4rem,env(safe-area-inset-top))] lg:pl-[max(100px,env(safe-area-inset-left))] flex-1 gap-16 bg-background lg:pt-0 min-h-[100dvh] max-h-screen overflow-scroll font-[family-name:var(--font-inter)]">
+        <main className="pb-[max(1rem,env(safe-area-inset-bottom))] pt-[max(4rem,env(safe-area-inset-top))] lg:pl-[max(100px,env(safe-area-inset-left))] flex-1 bg-background lg:pt-0 h-[100dvh] font-[family-name:var(--font-inter)]">
       <div className="flex flex-col gap-10 p-4 lg:p-10 h-full overflow-y-auto custom-scrollbar">
         <ScreenHeader heading={heading} />
         {children}
