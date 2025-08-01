d2cd509359ae531387b9ac8b6cde7335b314b131
Dominic Black <77794795+Dec0yyy@users.noreply.github.com>
2025-08-01 20:20:28 +1000

    restyle login and signup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import { AnimatedBackground } from "@/components/ui/AnimatedBackground/AnimatedB
 
 export default function Login() {
   return (
-    <div className="pb-[max(3rem,env(safe-area-inset-bottom))] relative pl-[max(1rem,env(safe-area-inset-left))] pr-[max(1rem,env(safe-area-inset-right))] pt-[max(3rem,env(safe-area-inset-top))] flex justify-center items-center px-4 sm:px-6 lg:px-8 py-12 min-h-[100dvh] min-h-screen">
+    <div className="relative pl-[max(1rem,env(safe-area-inset-left))] pr-[max(1rem,env(safe-area-inset-right))] pt-[max(3rem,env(safe-area-inset-top))] flex justify-center items-center px-4 sm:px-6 lg:px-8 py-12 h-[100dvh]">
       <AnimatedBackground />
       <div className="z-10 relative space-y-8 bg-white/60 shadow-xl backdrop-blur-lg p-8 sm:p-10 border border-white/50 rounded-2xl sm:rounded-3xl w-full max-w-md">
         <div>
